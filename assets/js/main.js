a07cb57c2d9a499d78e7d88932c5eaaf9e8cdc3f
Lorena <lorena.n.barrioz@gmail.com>
2024-11-06 01:09:02 -0300

    nuevos cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,167 +1,60 @@
 $(document).ready(() => {
-    const urls  = [
-        "https://swapi.dev/api/people/1/",
-        "https://swapi.dev/api/people/2/",
-        "https://swapi.dev/api/people/3/",
-        "https://swapi.dev/api/people/4/",
-        "https://swapi.dev/api/people/5/",
+    async function* personajes(inicio, final) {
+        for(let id = inicio; id <= final; id++ ) {
+            const url =`https://swapi.dev/api/people/${id}/`;
+            try {
+                const respuesta = await fetch (url);
+                if (!respuesta.ok) {
+                    throw new Error(`Hubo algún error al obtener el personaje`);
+                }
+                yield await respuesta.json();
+            } catch (error) {
+                console.log('Hubo algín error con el personaje:', error);
+                continue;
+            }
+        }
+    }
+
+    async function loadcharacters(inicio, final) {
+        const generator = personajes(inicio, final);
+        let done = false;
+        while(!done) {
+            const {value, done: isDone} = await generator.next();
+            done = isDone;
+            if (value) {
+                const data = value;
+                let contenidoHTML = crearcard(data)
+                $("#contenedor-personajes").append(`<h3> Personajes ${inicio} - ${final} </h3>`);
+                $("#contenedor-personajes").append(contenidoHTML);
+            }
+        }
+        $("#contenedor-personajes").addClass('visible');
+    }
+    const rangos = [ 
+        {inicio:1, final: 5, label: "Personajes principales", Class:"btn-uno"},
+        {inicio:6, final: 11, label: "Personajes secundarios", Class:"btn-dos"},
+        {inicio:12, final: 17, label: "Personajes Significativos", Class:"btn-tres"},
     ];
+    
+    for(let i = 0; i < rangos.length; i++){
+        const {inicio, final, label, Class} = rangos[i];
+        const boton = $('<button>')
+                    .text(label)
+                    .addClass(Class)    
+                    .click(()=> {
+            $("#contenedor-personajes").empty();
+            loadcharacters(inicio, final)
+        });
+        $("#botones").append(boton)
+    }
 
     const crearcard = ({name, height, mass}) => {
         return`
-        <div class="card">
-            <h2>${name}</h2>  
-            <p>Altura: ${height}</p>      
-            <p>Peso: ${mass}</p>    
-        </div>
-        `;
-    }; 
-    $("#load-characters").click(() => {
-        $("#contenedor-personajes").empty();
-
-        urls.forEach(url => {
-            fetch(url)
-                .then(respuesta => {
-                    if (!respuesta.ok) {
-                        throw new Error('Error en la respuesta');
-                        }
-                        return respuesta.json();
-                        })
-                        .then(data => {
-                            let contenidoHTML = crearcard(data);
-                            $("#contenedor-personajes").append(contenidoHTML); 
-                        })
-                        .catch(error => {
-                            console.log('Hubo un error:', error);
-                        });
-                });
-                $("#contenedor-personajes").addClass('visible'); 
-            });
-        });
-
-    // Segundos personajes
-    $(document).ready(() => {
-        const urls  = [
-            "https://swapi.dev/api/people/6/",
-            "https://swapi.dev/api/people/7/",
-            "https://swapi.dev/api/people/8/",
-            "https://swapi.dev/api/people/9/",
-            "https://swapi.dev/api/people/10/",
-        ];
-    
-        const crearcard = ({name, height, mass}) => {
-            return`
-            <div class="card2">
-                <h2>${name}</h2>  
-                <p>Altura: ${height}</p>      
-                <p>Peso: ${mass}</p>    
+            <div class="card">
+            <h2>${name}</h2>
+            <p>Altura: ${height}</p>
+            <p>Peso: ${mass}</p>
             </div>
             `;
-        }; 
-        $("#load-characters").click(() => {
-            $("#contenedor-personajes2").empty();
-    
-            urls.forEach(url => {
-                fetch(url)
-                    .then(respuesta => {
-                        if (!respuesta.ok) {
-                            throw new Error('Error en la respuesta');
-                            }
-                            return respuesta.json();
-                            })
-                            .then(data => {
-                                let contenidoHTML = crearcard(data);
-                                $("#contenedor-personajes2").append(contenidoHTML); 
-                            })
-                            .catch(error => {
-                                console.log('Hubo un error:', error);
-                            });
-                    });
-                    $("#contenedor-personajes2").addClass('visible'); 
-                });
-            });
-    
-// tercer personajes 
-$(document).ready(() => {
-    const urls  = [
-        "https://swapi.dev/api/people/11/",
-        "https://swapi.dev/api/people/12/",
-        "https://swapi.dev/api/people/13/",
-        "https://swapi.dev/api/people/14/",
-        "https://swapi.dev/api/people/15/",
-    ];
-
-    const crearcard = ({name, height, mass}) => {
-        return`
-        <div class="card3">
-            <h2>${name}</h2>  
-            <p>Altura: ${height}</p>      
-            <p>Peso: ${mass}</p>    
-        </div>
-        `;
-    }; 
-    $("#load-characters").click(() => {
-        $("#contenedor-personajes3").empty();
-
-        urls.forEach(url => {
-            fetch(url)
-                .then(respuesta => {
-                    if (!respuesta.ok) {
-                        throw new Error('Error en la respuesta');
-                        }
-                        return respuesta.json();
-                        })
-                        .then(data => {
-                            let contenidoHTML = crearcard(data);
-                            $("#contenedor-personajes3").append(contenidoHTML); 
-                        })
-                        .catch(error => {
-                            console.log('Hubo un error:', error);
-                        });
-                });
-                $("#contenedor-personajes3").addClass('visible'); 
-            });
-        });
-
-        // cuartos personajes 
-$(document).ready(() => {
-    const urls  = [
-        "https://swapi.dev/api/people/16/",
-        "https://swapi.dev/api/people/17/",
-        "https://swapi.dev/api/people/18/",
-        "https://swapi.dev/api/people/19/",
-        "https://swapi.dev/api/people/20/",
-        "https://swapi.dev/api/people/21/",
-    ];
-
-    const crearcard = ({name, height, mass}) => {
-        return`
-        <div class="card4">
-            <h2>${name}</h2>  
-            <p>Altura: ${height}</p>      
-            <p>Peso: ${mass}</p>    
-        </div>
-        `;
-    }; 
-    $("#load-characters").click(() => {
-        $("#contenedor-personajes4").empty();
-
-        urls.forEach(url => {
-            fetch(url)
-                .then(respuesta => {
-                    if (!respuesta.ok) {
-                        throw new Error('Error en la respuesta');
-                        }
-                        return respuesta.json();
-                        })
-                        .then(data => {
-                            let contenidoHTML = crearcard(data);
-                            $("#contenedor-personajes4").append(contenidoHTML); 
-                        })
-                        .catch(error => {
-                            console.log('Hubo un error:', error);
-                        });
-                });
-                $("#contenedor-personajes4").addClass('visible'); 
-            });
-        });
+    };
+})
